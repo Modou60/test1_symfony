057e3805280eaecb7a26553df13bbaf3ccd861e6
modou <touba28marche@laposte.net>
2021-11-10 19:18:04 +0100

    location qui ne s'affiche pas

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Location;
 use App\Form\LocationType;
 use App\Repository\LocationRepository;
-
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use symfony\Component\HttpFoundation\Request;
@@ -13,9 +12,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 /**
  * @Route("/location")
  */
+
 class LocationController extends AbstractController
 {
     /**
@@ -23,55 +24,56 @@ class LocationController extends AbstractController
      */
     public function index(): Response
     {
-        // création de la requête de toutes les locations pour être affichées
-    
+        // je fais une requette dans la base pour avoir toutes les locations
         $repo = $this->getDoctrine()->getRepository(Location::class);
         $location = $repo->findAll();
 
-        // envoi vers la page twig pour être affichée
-        return $this->render('location/index.html.twig',[
+        // J'envoie le résultat vers la page twig
+        return $this->render('location/index.html.twig', [
             'locations' => $location,
         ]);
     }
 
-    /**
-     * @Route("/locform", name="locform")
-     */
-    public function formlocation(Request $request): Response
+
+/**
+ * @Route("/locform", name="locform")
+ */
+public function formlocation(Request $request, EntityManagerInterface $entityManagerInterface, EntityManager $manager): Response
+{
+    // instanciation de la classe Location
+    $location = new Location;
+    // Cration d'un formulaire
+    $formloc = $this->createForm(LocationType::class, $location);
+    $formloc->handleRequest($request);
+
+    // Test pour la validité du formulaire et sa persistance
+    if ($formloc->isSubmitted() && $formloc->isValid())
     {
-        $location = new Location;
+    // $manager = $this->getDoctrine()->getManager();
+    $manager->persist($location);
+    $manager->flush();
 
-        // création d'un formulaire
-        $formlocation = $this->createForm(LocationType::class, $location);
-        $formlocation->handleRequest($request);
+   // redirection de la page après  persistance
+    return $this->redirectToRoute('location');
+}
 
-        // test pour la validité du formulaire et sa persistance
-        if ($formlocation->isSubmitted() && $formlocation->isValid())
-        {
-            $manager = $this->getDoctrine()->getManager();
-           $manager->persist($location);
-            $manager->flush();
+// envoi de la page vers twig pour son affichage
+return $this->render('location/locformulaire.html.twig',[
+    'location' => $location,
+    'locationform' => $formloc->createView(),
+]);
+}
 
-            // redirection de la page après persistance
-            return $this->redirectToRoute('location');
-        }
 
-        // envoi de la page vers twig pour son affichage
-        return $this->render('location/locformulaire.html.twig',[
-            'locations' => $location,
-            'locationform' => $formlocation->createView(),
-        ]);
-
-    }
-
-    // affichage d'une location donnée
-    /**
-     * @Route("/{id}", name="location_id", methods={"GET"})
-     */
-    public function idlocation(Location $location)
+// Affichage d'une location donnée
+/**
+ * @Route("/{id}", name="location_id", methods={"GET"})
+ */
+public function idlocation(Location $location): Response
     {
 return $this->render('location/affichelocation.html.twig',[
     'loc' => $location,
 ]);
     }
+
 }
--- src/Form/LocationType.php
+++ src/Form/LocationType.php
@@ -3,18 +3,20 @@
 namespace App\Form;
 
 use App\Entity\Location;
-
+use DateTime;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RadioType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
 
 class LocationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('date')
+            ->add('date')
             ->add('titre')
             ->add('categorie')
             ->add('description')
@@ -22,7 +24,8 @@ class LocationType extends AbstractType
             ->add('adresse')
             ->add('accessibility')
             ->add('alaune')
-            ->add('image');
+            ->add('image')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -31,4 +34,4 @@ class LocationType extends AbstractType
             'data_class' => Location::class,
         ]);
     }
-    }
+}
